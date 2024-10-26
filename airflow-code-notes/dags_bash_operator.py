11807b2d28a1bc38b5c8a49c1137031b7abd6d6d
dasollee2525 <dasollee2525@gmail.com>
2024-10-26 21:13:11 +0900

    Add airflow-code-notes as a regular directory

---
+++ airflow-code-notes/dags_bash_operator.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+import pendulum
+import datetime
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 10, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["2024-10-26"]
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoareyou",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash t1 >> bash t2
