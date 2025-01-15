21ac027c58638dd2aa128947d722ba78f076b558
dasollee2525 <dasollee2525@gmail.com>
2025-01-16 03:28:37 +0900

    xcom and branch

---
+++ airflow-code-notes/dags_bash_with_xcom_eg1.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg1",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023,3,1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_command="echo START && "
+                     "echo XCOM_PUSHED "
+                     "{{ti.xcom_push(key='bash_pushed', value='first_bash_message')}} && "
+                     "echo COMPLETE"  
+                     # 마지막 값을 출력값으로 인지함 
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env={
+            'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed)}}",
+            'RETURN_VALUE':"{{ti.xcom_pull(task_ids='bash_push)}}"
+        },
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
+        do_xcom_push=False
+    )
+
+    bash_push >> bash_pull
+
